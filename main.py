85223cb27580930e8b8d27a75da1ea68f82c147d
icarus <ellishogan95@gmail.com>
2019-12-04 21:13:26 -0500

    update

--- main.py
+++ main.py
@@ -36,16 +36,17 @@ def pokemon(pokemon):
 	req = requests.get(f'https://pokeapi.co/api/v2/pokemon/{pokemon}').json()
 	for i in req['stats']:
 		print(f"{i['stat']['name']}: {i['base_stat']}")
-	
 	for i in req['types']:
 		print(f"type: {i['type']['name']}")
 	for i in req['sprites']:
 		print(f"{' '.join(i.split('_'))}: {req['sprites'][i]}")
 	stats = req['stats']
 	types = req['types']
-	sprites = req['sprites']
+	sprites = [req['sprites'][i] for i in req['sprites']]
 	name = req['name']
+	name = name.capitalize()
 	weight = req['weight']
+	sprites[0], sprites[1], sprites[2], sprites[3], sprites[4], sprites[5], sprites[6], sprites[7] = sprites[4], sprites[0], sprites[5], sprites[1], sprites[6], sprites[2], sprites[7], sprites[3]
 	return render_template('pokemon.html', stats = stats, types = types, sprites = sprites, name = name, weight = weight)
 	
 @app.route('/get_pokemon', methods=['POST'])
